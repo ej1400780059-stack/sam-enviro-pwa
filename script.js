9e319fcc468eaac14c908687eadd918d920a9995
ej1400780059-stack <ej1400780059@gmail.com>
2026-07-06 16:17:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+/* ==========================================
+   SAM ENVIRO
+   Homepage Script
+========================================== */
+
+/* ==========================================
+   GOOGLE APPS SCRIPT WEB APP URLS
+========================================== */
+
+const ETP_URL =
+"https://script.google.com/macros/s/AKfycbyL5YI8v7Fyr5k16tt6rAvj0AkXfzr9Kv-DLfQNR3qyix64UoA-KB2A3VbznwPXjljO/exec";
+
+const STP_URL =
+"https://script.google.com/macros/s/AKfycbzXvY4yFLel8pLV1qEJGjjew7MLThFfVrpOvdjtFz_DaXeG7Ms_P0FhAzUDizBHTO-8uQ/exec";
+
+const ETPRO_URL =
+"https://script.google.com/macros/s/AKfycbx_jQaQymr6mdrml5VowoGryk_jvgcB-RSVXZWG66jo_ga2kZAPS41bJqq4sFAayK9_/exec";
+
+
+/* ==========================================
+   OPEN REPORT FORMS
+========================================== */
+
+function openETP() {
+
+    window.location.href = ETP_URL;
+
+}
+
+function openSTP() {
+
+    window.location.href = STP_URL;
+
+}
+
+function openETPRO() {
+
+    window.location.href = ETPRO_URL;
+
+}
+
+
+/* ==========================================
+   PAGE LOADED
+========================================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("SAM ENVIRO Dashboard Loaded");
+
+});
